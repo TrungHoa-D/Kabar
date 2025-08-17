0177b1a7cf2b99099830422cea2b3ca362a4d8e3
ndthuong13 <thuongnguyendinh959@gmail.com>
2025-08-17 09:05:16 +0700

    feat: article details

--- app/src/main/java/com/example/socialnetwork/utils/constant/APIConstant.java
+++ app/src/main/java/com/example/socialnetwork/utils/constant/APIConstant.java
@@ -1,11 +1,18 @@
 package com.example.socialnetwork.utils.constant;
 
-public class APIConstant {
+public final class APIConstant {
     public static final String BASE_URL = "https://your-api-url.com/";
+
+    private APIConstant() {
+
+    }
 
     public static class TimeOut {
         public static final int CONNECT_TIME_OUT = 30;
         public static final int READ_TIME_OUT = 30;
         public static final int WRITE_TIME_OUT = 30;
     }
+
+
+
 }
